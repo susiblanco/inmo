e4ce05085f77bbd3637ce85a618acd0d49aa029b
susi blanco <susiblanco44@gmail.com>
2013-05-17 08:04:37 -0300

    cambio en los models

---
+++ src/main/java/com/unrc/app/models/Admin.java
@@ -0,0 +1,5 @@
+package com.unrc.app.models;
+
+import org.javalite.activejdbc.Model;
+
+public class Admin extends Model {}
---
+++ src/main/java/com/unrc/app/models/Owner.java
@@ -0,0 +1,5 @@
+package com.unrc.app.models;
+
+import org.javalite.activejdbc.Model;
+
+public class Owner extends Model { }
